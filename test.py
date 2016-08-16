8205496138a021cc35281fac05bc742d0c737a91
J3S <johnnysuarez@outlook.com>
2016-08-16 16:21:54 -0500

    Cambio del nombre de funciones de los tests

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ class pruebas(unittest.TestCase):
 
     """ Todos los datos correctos, Libro tiene categoria CE,
           la fecha tiene que sumarse 7 dias """
-    def test_function_1(self):
+    def test_function_2(self):
         tarjeta = Tarjeta("AA", "BB", "1234567", 10)
         libro = Libro("","CE",0)
         biblioteca = Biblioteca()
@@ -25,7 +25,7 @@ class pruebas(unittest.TestCase):
         self.assertEqual(valor1, valor2)
    
     """ Tarjeta inválida, retorna 0"""
-    def test_function_1(self):
+    def test_function_3(self):
         tarjeta = Tarjeta("AA", "BB", "003456", 10)
         libro = Libro("","CE",0)
         biblioteca = Biblioteca()
@@ -34,7 +34,7 @@ class pruebas(unittest.TestCase):
         self.assertEqual(valor1, valor2)
 
     """ Categoŕía del libro inválida, retorna 0"""
-    def test_function_1(self):
+    def test_function_4(self):
         tarjeta = Tarjeta("AA", "BB", "003456", 10)
         libro = Libro("","CEN",0)
         biblioteca = Biblioteca()
@@ -43,13 +43,14 @@ class pruebas(unittest.TestCase):
         self.assertEqual(valor1, valor2)
 
     """ Libro no disponible, retorna 0"""
-    def test_function_1(self):
+    def test_function_5(self):
         tarjeta = Tarjeta("AA", "BB", "003456", 10)
         libro = Libro("","CE",1)
         biblioteca = Biblioteca()
         valor1 = biblioteca.prestar_libro(libro,tarjeta,"16/08/2016")
         valor2 = 0
         self.assertEqual(valor1, valor2)
+
 
 if __name__ == '__main__':
     unittest.main()
