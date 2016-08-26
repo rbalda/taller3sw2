52b0dda106578df0c24c460f58cffb907a8a9d09
Rene Balda <rbalda@espol.edu.ec>
2016-08-26 06:43:56 -0500

    agregando pruebas

--- tests.py
+++ tests.py
@@ -50,13 +50,37 @@ class ProgramaTest(unittest.TestCase):
         tarjeta = Tarjeta("AAA", "AAA", "2212345", 1)
         valor1 = Bus.cobrar_pasaje(tarjeta, 'a')
         valor2 = 0b0
-    def test_function_9(self):
 
-    def test_function_10(self):
+    def setUp(self):
+        self.edificio= Edificio()
+        self.bus = Bus()
 
-    def test_function_11(self):
+    def test_accede_trabajador_horario_normal(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,5,0) == 0b1
 
-    def test_function_12(self):
-    #Pruebas para las funcionalidades de la biblioteca
-    def test_biblio_1(self):
-        tarjeta =
+    def test_accede_trabajador_fin_semana(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,15) == 0b1
+
+    def test_trabajador_no_accede(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,16) == 0b0
+
+    def test_accede_estudiante(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,18)==0b1
+
+    def test_estudiante_no_accede(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,19)==0b0
+
+    def test_tarjeta_invalida(self):
+        tarjeta = Tarjeta("Manuel","Torres","11123a5",1)
+        assert self.edificio.conceder_acceso(tarjeta,5,19)==0b0
+
+    def test_tarjeta_none(self):
+        assert self.edificio.conceder_acceso(None,5,19) == 0b0
+
+if __name__ == '__main__':
+    unittest.main()
