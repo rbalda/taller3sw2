be79b83612cae4c5d969373fc1e332b5c72e8323
Rene Balda <rbalda@espol.edu.ec>
2016-08-26 04:57:43 -0500

    agregando pruebas

--- tests.py
+++ tests.py
@@ -1,11 +1,39 @@
-from programa import Biblioteca, Libro, Validador, Tarjeta
-from datetime import date, timedelta
+from programa import Biblioteca, Libro, Validador, Tarjeta,Edificio,Bus
+from datetime import date, timedelta,datetime
 
 import unittest
 
 class Test_System(unittest.TestCase):
     def setUp(self):
-        pass
+        self.edificio= Edificio()
+        self.bus = Bus()
+
+    def test_accede_trabajador_horario_normal(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,5,0) == 0b1
+
+    def test_accede_trabajador_fin_semana(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,15) == 0b1
+
+    def test_trabajador_no_accede(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,16) == 0b0
+
+    def test_accede_estudiante(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,18)==0b1
+
+    def test_estudiante_no_accede(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,19)==0b0
+
+    def test_tarjeta_invalida(self):
+        tarjeta = Tarjeta("Manuel","Torres","11123a5",1)
+        assert self.edificio.conceder_acceso(tarjeta,5,19)==0b0
+
+    def test_tarjeta_none(self):
+        assert self.edificio.conceder_acceso(None,5,19) == 0b0
 
     def test_categoria_valida(self):
         libro = Libro(categoria="CS")
@@ -27,15 +55,15 @@ class Test_System(unittest.TestCase):
     def test_prestamo_libro_valido_7_dias(self):
         libro = Libro(categoria="CE")
         tarjeta = Tarjeta(nombres="Daniel",apellidos="Izquierdo",codigo="1234567",saldo_inicial=0.5)
-        prestamo_libro = Biblioteca.prestar_libro(libro=libro,tarjeta=tarjeta)
-        fecha_esperada_devolucion = (date.today() + timedelta(days=7)).strftime('%d/%m/%Y')
+        prestamo_libro = Biblioteca.prestar_libro(libro=libro,tarjeta=tarjeta,fecha_actual=datetime.strptime("16/08/2016","%d/%m/%Y"))
+        fecha_esperada_devolucion = (datetime.strptime("16/08/2016","%d/%m/%Y") + timedelta(days=7)).strftime('%d/%m/%Y')
         self.assertEqual(prestamo_libro,fecha_esperada_devolucion)
 
     def test_prestamo_libro_valido_14_dias(self):
         libro = Libro(categoria="CH")
         tarjeta = Tarjeta(nombres="Daniel",apellidos="Izquierdo",codigo="1234567",saldo_inicial=0.5)
-        prestamo_libro = Biblioteca.prestar_libro(libro=libro,tarjeta=tarjeta)
-        fecha_esperada_devolucion = (date.today() + timedelta(days=14)).strftime('%d/%m/%Y')
+        prestamo_libro = Biblioteca.prestar_libro(libro=libro,tarjeta=tarjeta,fecha_actual=datetime.strptime("16/08/2016","%d/%m/%Y"))
+        fecha_esperada_devolucion = (datetime.strptime("16/08/2016","%d/%m/%Y") + timedelta(days=14)).strftime('%d/%m/%Y')
         self.assertEqual(prestamo_libro,fecha_esperada_devolucion)
 
 if __name__ == '__main__':
