ed828f76c311456c52fa8dad29e82f5389287809
Rene Balda <rbalda@espol.edu.ec>
2016-08-26 05:23:24 -0500

    arreglando pruebas

--- prueba.py
+++ prueba.py
@@ -1,7 +1,7 @@
 import unittest
 #from codigo import Control
 #from pasaje import Pasaje
-from programa import Bus, Biblioteca
+from programa import Bus, Biblioteca, Edificio,Tarjeta
 
 
 class AddTest(unittest.TestCase):
@@ -12,11 +12,40 @@ class AddTest(unittest.TestCase):
       #self.tarjeta = Tarjeta()
       self.codigo2 = Bus()
       self.biblioteca = Biblioteca()
-      
+      self.edificio= Edificio()
+      self.bus = Bus()
+
       pass
     def tearDown(self):
       pass
 
+
+    def test_accede_trabajador_horario_normal(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,5,0) == 0b1
+
+    def test_accede_trabajador_fin_semana(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,15) == 0b1
+
+    def test_trabajador_no_accede(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,16) == 0b0
+
+    def test_accede_estudiante(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,18)==0b1
+
+    def test_estudiante_no_accede(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,19)==0b0
+
+    def test_tarjeta_invalida(self):
+        tarjeta = Tarjeta("Manuel","Torres","11123a5",1)
+        assert self.edificio.conceder_acceso(tarjeta,5,19)==0b0
+
+    def test_tarjeta_none(self):
+        assert self.edificio.conceder_acceso(None,5,19) == 0b0
     """
     def test_codigoEmpiezaDobleCero(self):
         self.assertEqual(self.codigo.valor_acceso('0012345',1, 1),None)
@@ -37,7 +66,7 @@ class AddTest(unittest.TestCase):
     def test_diaEstudiante3(self):
         self.assertEqual(self.codigo.valor_acceso('0021234',6, 14),None)
     def test_cobrarPasajeLunes_Si_saldo(self):
-        self.assertEqual(self.pasaje.valor_cobrar(1,0.25,1),1)    
+        self.assertEqual(self.pasaje.valor_cobrar(1,0.25,1),1)
     def test_cobrarPasaje_Martes_NO_Saldo(self):
         self.assertEqual(self.pasaje.valor_cobrar(1,0.25,0),0)
     def test_cobrarPasaje_Viernes(self):
