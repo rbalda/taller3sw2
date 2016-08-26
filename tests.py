2bf2cc696afe387d9c3a81186e92b39c0037ea84
Rene Balda <rbalda@espol.edu.ec>
2016-08-26 04:00:57 -0500

    agregando pruebas

--- tests.py
+++ tests.py
@@ -19,9 +19,40 @@ def test_crear_paciente(self):
 from programa import Biblioteca
 from programa import Libro
 from programa import Tarjeta
+from programa import Edificio
 
 
 class ProbarSistemaAcceso(unittest.TestCase):
+    def setUp(self):
+        self.edificio= Edificio()
+        self.bus = Bus()
+
+    def test_accede_trabajador_horario_normal(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,5,0) == 0b1
+
+    def test_accede_trabajador_fin_semana(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,15) == 0b1
+
+    def test_trabajador_no_accede(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,16) == 0b0
+
+    def test_accede_estudiante(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,18)==0b1
+
+    def test_estudiante_no_accede(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,19)==0b0
+
+    def test_tarjeta_invalida(self):
+        tarjeta = Tarjeta("Manuel","Torres","11123a5",1)
+        assert self.edificio.conceder_acceso(tarjeta,5,19)==0b0
+
+    def test_tarjeta_none(self):
+        assert self.edificio.conceder_acceso(None,5,19) == 0b0
 
     def test_escenario1(self):
         tarj1 = Tarjeta(nombres="Carlos", apellidos="Piguabe", codigo=0012345, saldo_inicial=20)
