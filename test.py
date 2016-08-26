e70a835e32323d31d0c7090c753a220010f4e906
Rene Balda <rbalda@espol.edu.ec>
2016-08-26 06:11:32 -0500

    agregando test

--- test.py
+++ test.py
@@ -3,6 +3,38 @@ from programa import *
 
 
 class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.edificio= Edificio()
+        self.bus = Bus()
+
+    def test_accede_trabajador_horario_normal(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,5,0) == 0b1
+
+    def test_accede_trabajador_fin_semana(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,15) == 0b1
+
+    def test_trabajador_no_accede(self):
+        trabajador = Tarjeta("Juan","De la Cruz","0012345",0.50)
+        assert self.edificio.conceder_acceso(trabajador,6,16) == 0b0
+
+    def test_accede_estudiante(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,18)==0b1
+
+    def test_estudiante_no_accede(self):
+        estudiante = Tarjeta("Manuel","Torres","1112345",1)
+        assert self.edificio.conceder_acceso(estudiante,5,19)==0b0
+
+    def test_tarjeta_invalida(self):
+        tarjeta = Tarjeta("Manuel","Torres","11123a5",1)
+        assert self.edificio.conceder_acceso(tarjeta,5,19)==0b0
+
+    def test_tarjeta_none(self):
+        assert self.edificio.conceder_acceso(None,5,19) == 0b0
+        
     def test1(self):
         l = Libro('CE', 0)
         t = Tarjeta('Angely', 'Oyola', '1012345', 1)
